820eb666edafd31ab9900c97c3155f142c1833eb
Thiago Felipe Alves do Carmo <thiagofelipealves13@gmail.com>
2025-02-27 17:17:14 -0300

    Colors update

--- config.fish
+++ config.fish
@@ -1,6 +1,7 @@
 set -U fish_greeting
 export EDITOR
 alias vimconfig="nvim ~/.config/nvim"
+alias ls="eza"
 
 function temple
     cd ~/Documents/Oracle\'s\ Temple
@@ -17,5 +18,5 @@ function newtmux
 end
 starship init fish | source
 
-# Created by `pipx` on 2025-02-18 01:50:03
 set PATH $PATH /Users/oracleofdelfos/.local/bin
+set -x LS_COLORS "di=1;94:ln=1;36:so=1;35:pi=1;33:ex=1;97:bd=1;38;5;110:cd=1;38;5;110:su=1;31:sg=1;31:tw=1;34:ow=1;34:fi=38;5;67:*.c=1;36:*.rb=1;36:*.ele=1;36:*.el=1;36:*.ipynb=1;36:*.py=1;36:*.log=38;5;67"
