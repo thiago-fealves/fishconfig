3ce76015118013de13f36432566b19b029cc0719
Thiago Felipe Alves do Carmo <thiagofelipealves13@gmail.com>
2025-02-26 23:42:03 -0300

    Update 1.0

--- config.fish
+++ config.fish
@@ -6,10 +6,16 @@ function temple
     cd ~/Documents/Oracle\'s\ Temple
 end
 
+function github
+  cd ~/Documents/GitHub
+end
+
 function newtmux
     tmux new -s $argv[1] \; detach
     tmux source-file ~/tmux.conf
     tmux attach-session -t $argv[1]
 end
-
 starship init fish | source
+
+# Created by `pipx` on 2025-02-18 01:50:03
+set PATH $PATH /Users/oracleofdelfos/.local/bin
