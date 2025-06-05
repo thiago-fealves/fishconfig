af2fa218f831075e09ad75e3d0e78052da80686e
thiago-fealves <thiagofelipealves13@gmail.com>
2025-06-04 21:03:08 -0300

    small changes

--- config.fish
+++ config.fish
@@ -1,8 +1,17 @@
 set -U fish_greeting
-export EDITOR
+starship init fish | source
+fzf --fish | source
+zoxide init fish | source
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+
+bind \cf fzf-cd-widget
 alias vimconfig="nvim ~/.config/nvim"
 alias ls="eza"
+alias vi="nvim"
+alias inv='nvim $(fzf -m --preview="bat --color=always {}")'
 alias fastfetch="fastfetch --logo-color-1 cyan --color-keys cyan --color-title blue"
+alias kali="docker start -ai kali-pentest"
 
 function temple
     cd ~/Documents/Oracle\'s\ Temple
@@ -14,12 +23,15 @@ function github
   cd ~/Documents/GitHub
 end
 
-function newtmux
-    tmux new -s $argv[1] \; detach
-    tmux source-file ~/tmux.conf
-    tmux attach-session -t $argv[1]
-end
-starship init fish | source
-
 set PATH $PATH /Users/oracleofdelfos/.local/bin
 set -x LS_COLORS "di=1;94:ln=1;36:so=1;35:pi=1;33:ex=1;97:bd=1;38;5;110:cd=1;38;5;110:su=1;31:sg=1;31:tw=1;34:ow=1;34:fi=38;5;67:*.md=38;5;67:*.c=1;36:*.rb=1;36:*.ele=1;36:*.el=1;36:*.ipynb=1;36:*.py=1;36:*.cpp=1;36:*.lua=1;36:*.log=38;5;:*.str=92:"
+
+if status is-interactive
+    if not set -q TMUX
+        if not tmux has-session -t Terminal 2> /dev/null
+            tmux new-session -d -s Terminal
+        end
+
+        tmux attach-session -t Terminal
+    end
+end
